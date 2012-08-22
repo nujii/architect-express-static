2187868b323add063ce8d2d0695b84eb4aa71f64
Cameron Bytheway <cameron@nujii.com>
2012-08-22 10:36:11 -0600

    Added logger dependency

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ module.exports = function startup(options, imports, register) {
 
   var mountPath = options.mount || options.path;
 
-  if (browsable) {
+  if (options.browsable) {
     imports.express.use(mountPath, express.directory(options.path));
   }
   else {
     imports.express.use(mountPath, express.static(options.path));
   }
 
-  imports.logger.debug('Static path registered at '+path+
+  imports.logger.debug('Static path registered at '+options.path+
                        ' and mounted at '+mountPath);
+  register(null, {});
 };
